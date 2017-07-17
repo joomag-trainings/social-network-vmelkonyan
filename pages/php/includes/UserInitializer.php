45232c289065bdf45a5c8733f0006542c8774918
holymoof <melkonyanmail@gmail.com>
2017-07-17 04:29:19 +0400

    Create class UserInitializer that incapsulates method for creating new User class object and initializing it with values validated by RegistrationValidator class object

---
+++ pages/php/includes/UserInitializer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moof
+ * Date: 7/17/17
+ * Time: 3:59 AM
+ */
+
+namespace php\inc;
+
+
+class UserInitializer
+{
+    private $newUser;
+
+    public function initializeUser($firstName, $lastName, $pseudonym, $email, $password)
+    {
+        $validator = new RegistrationValidator();
+        $newUser = new User();
+        $newUser->setFirstName($validator->validateFirstName($firstName));
+        $newUser->setLastName($validator->validateLastName($lastName));
+        $newUser->setPseudonym($validator->validatePseudonym($pseudonym));
+        $newUser->setEmail($validator->validateEmail($email));
+        $newUser->setPassword($validator->validatePassword($password));
+        return $newUser;
+    }
+}
