b76118a3a1b84a535a16d841f91276dc4289a8ee
holymoof <melkonyanmail@gmail.com>
2017-07-17 04:32:11 +0400

    Create interface consisting of validate function Is used in several classes

---
+++ pages/php/includes/DataValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: moof
+ * Date: 7/17/17
+ * Time: 3:50 AM
+ */
+
+namespace php\inc;
+
+interface DataValidator
+{
+    public function validate($data);
+}
