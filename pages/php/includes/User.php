7cff09e2dad249d20040c71a80b4d5825bfcd902
holymoof <melkonyanmail@gmail.com>
2017-07-17 04:28:19 +0400

    Create class User that incapsulates personal information, get and set methods

---
+++ pages/php/includes/User.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moof
+ * Date: 7/17/17
+ * Time: 3:14 AM
+ */
+
+namespace php\inc;
+
+class User
+{
+    private $firstName;
+    private $lastName;
+    private $pseudonym;
+    private $email;
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPseudonym()
+    {
+        return $this->pseudonym;
+    }
+
+    /**
+     * @param mixed $pseudonym
+     */
+    public function setPseudonym($pseudonym)
+    {
+        $this->pseudonym = $pseudonym;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+}
+
